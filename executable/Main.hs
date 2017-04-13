86dc53bfad1b7db41f56864126a560eb94f5266a
Nicu Ionita <nicu@ionita.at>
2017-04-13 12:26:28 +0200

    Mutation logic implemented (add connection / node)

--- executable/Main.hs
+++ executable/Main.hs
@@ -1,11 +1,15 @@
 -- It is generally a good idea to keep all your business logic in your library
 -- and only use it in the executable. Doing so allows others to use what you
 -- wrote in their libraries.
+import Control.Monad.Random
 import Genome
 import Network
 
 main :: IO ()
-main = do
+main = main2
+
+main1 :: IO ()
+main1 = do
   let genome = Genome {
                    inNodes = 3, outNodes = 1, hiddenNodes = 1,
                    genes = [gene1, gene2, gene3, gene4, gene5]
@@ -22,3 +26,21 @@ main = do
       out2 = myNet input2
   putStrLn $ "Result for input 1: " ++ show out1
   putStrLn $ "Result for input 2: " ++ show out2
+
+main2 :: IO ()
+main2 = do
+  let genome = Genome {
+                   inNodes = 3, outNodes = 1, hiddenNodes = 1,
+                   genes = [gene1, gene2, gene3, gene4, gene5]
+               }
+      gene1 = Gene { inNode = 1, outNode = 4, innov = 1, weight =  0.1, enabled = True }
+      gene2 = Gene { inNode = 1, outNode = 5, innov = 2, weight =  0.2, enabled = True }
+      gene3 = Gene { inNode = 2, outNode = 5, innov = 3, weight = -0.3, enabled = True }
+      gene4 = Gene { inNode = 5, outNode = 4, innov = 4, weight = -0.4, enabled = True }
+      gene5 = Gene { inNode = 3, outNode = 4, innov = 5, weight =  0.5, enabled = True }
+  genome2 <- evalRandTIO $ mutateAddConnection 6 genome
+  genome3 <- evalRandTIO $ mutateAddConnection 6 genome
+  genome4 <- evalRandTIO $ mutateAddConnection 6 genome
+  putStrLn $ "addConnection 1: " ++ show genome2
+  putStrLn $ "addConnection 2: " ++ show genome3
+  putStrLn $ "addConnection 3: " ++ show genome4
--- library/Genome.hs
+++ library/Genome.hs
@@ -1,28 +1,62 @@
 module Genome (
-    Gene(..), Genome(..)
+    Gene(..), Genome(..),
+    mutateAddConnection, mutateAddNode
 ) where
+
+import Control.Monad.Random
+import qualified Data.IntSet as S
 
 {--
 The genotype is represented as:
 - the number of sensors (input), output and hidden nodes (there is always a bias "input", always 1)
 - a list of connecton genes, describing the connections of the network and its weights
 
-Connections go from one node to another with the restriction that a sensor node cannot
-appear as a destination. Connections can form cycles (even with one single node).
+Connections go from one node to another with 2 restrictions:
+- a sensor node cannot appear as a destination
+- connections cannot form cycles
 --}
 
 data Gene = Gene {
                 inNode, outNode, innov :: Int,
                 weight :: Float,
                 enabled :: Bool
-          }
+          } deriving Show
 
 data Genome = Genome {
                   inNodes, outNodes, hiddenNodes :: Int,
                   genes :: [Gene]
-            }
+            } deriving Show
 
-{-
-mutateAddConnection :: (Int, Genome) -> (Int, Genome)
-mutateAddConnection (inno, genome)
--}
+-- We have to add connections in such a way that we do not create cycles
+mutateAddConnection :: (RandomGen g, Monad m) => Int -> Genome -> RandT g m (Maybe Genome)
+mutateAddConnection inno genome = do
+    let maf = inNodes genome + outNodes genome + hiddenNodes genome
+        mit = inNodes genome + 1
+    f <- getRandomR (0, maf)   -- every node can be source
+    t <- getRandomR (mit, maf) -- bias and inputs can't be destination
+    if f == t || (f, t) `isIn` (genes genome) || S.member f (closure (genes genome) t)
+       then return Nothing
+       else do
+           w <- getRandomR (-1, 1)  -- which range should we take?
+           let genome2 = genome { genes = genes genome ++ [gene] }
+               gene = Gene { inNode = f, outNode = t, innov = inno, weight = w, enabled = True }
+           return (Just genome2)
+
+isIn :: (Int, Int) -> [Gene] -> Bool
+isIn (i, j) gs = not $ null $ (filter ((== i) . inNode) . filter ((== j) . outNode)) gs
+
+closure :: [Gene] -> Int -> S.IntSet
+closure gs i = S.singleton i `S.union` S.unions ds
+    where ds = map (closure gs) $ map outNode $ filter ((== i) . inNode) $ filter enabled $ gs
+
+mutateAddNode :: (RandomGen g, Monad m) => Int -> Genome -> RandT g m Genome
+mutateAddNode inno genome = do
+    let cs = filter enabled (genes genome)
+    co <- uniform cs
+    let cd = co { enabled = False }
+        (cs1, cs2) = span ((/= innov co) . innov) (genes genome)
+        h = hiddenNodes genome + 1
+        gene1 = co { outNode = h, innov = inno, weight = 1 }
+        gene2 = co { inNode = h, innov = inno + 1 }
+        genome2 = genome { hiddenNodes = h, genes = cs1 ++ [cd] ++ tail cs2 ++ [gene1, gene2] }
+    return genome2
