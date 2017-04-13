ce2adde95a9910bc0455d74db386e141be7c8da4
Nicu Ionita <nicu@ionita.at>
2017-04-13 20:56:32 +0200

    Innovations reuse added, corrections

--- executable/Main.hs
+++ executable/Main.hs
@@ -29,24 +29,49 @@ main1 = do
 
 main2 :: IO ()
 main2 = do
-  let genome = Genome {
-                   inNodes = 3, outNodes = 1, hiddenNodes = 1,
-                   genes = [gene1, gene2, gene3, gene4, gene5]
-               }
-      gene1 = Gene { inNode = 1, outNode = 4, innov = 1, weight =  0.1, enabled = True }
-      gene2 = Gene { inNode = 1, outNode = 5, innov = 2, weight =  0.2, enabled = True }
-      gene3 = Gene { inNode = 2, outNode = 5, innov = 3, weight = -0.3, enabled = True }
-      gene4 = Gene { inNode = 5, outNode = 4, innov = 4, weight = -0.4, enabled = True }
-      gene5 = Gene { inNode = 3, outNode = 4, innov = 5, weight =  0.5, enabled = True }
-  genome2 <- evalRandTIO $ mutateAddConnection 6 genome
-  genome3 <- evalRandTIO $ mutateAddConnection 6 genome
-  genome4 <- evalRandTIO $ mutateAddConnection 6 genome
-  putStrLn $ "addConnection 1: " ++ show genome2
-  putStrLn $ "addConnection 2: " ++ show genome3
-  putStrLn $ "addConnection 3: " ++ show genome4
-  genome5 <- evalRandTIO $ mutateAddNode 6 genome
-  genome6 <- evalRandTIO $ mutateAddNode 8 genome5
-  genome7 <- evalRandTIO $ mutateAddNode 10 genome6
-  putStrLn $ "addNode 1: " ++ show genome5
-  putStrLn $ "addNode 2: " ++ show genome6
-  putStrLn $ "addNode 3: " ++ show genome7
+  let genome0 = Genome {
+                    inNodes = 3, outNodes = 1, hiddenNodes = 0, genes = []
+                }
+      i0  = 1
+      is0 = []
+  mr1 <- evalRandTIO $ mutateAddConnection i0 is0 genome0
+  let (i1, is1, genome1) = case mr1 of
+          Just r  -> r
+          Nothing -> (i0, is0, genome0)
+  mr2 <- evalRandTIO $ mutateAddConnection i1 is1 genome1
+  let (i2, is2, genome2) = case mr2 of
+          Just r  -> r
+          Nothing -> (i1, is1, genome1)
+  mr3 <- evalRandTIO $ mutateAddConnection i2 is2 genome2
+  let (i3, is3, genome3) = case mr3 of
+          Just r  -> r
+          Nothing -> (i2, is2, genome2)
+  putStrLn "addConnection 1:"
+  showGenome genome1
+  putStrLn "addConnection 2:"
+  showGenome genome2
+  putStrLn "addConnection 3:"
+  showGenome genome3
+  (i4, is4, genome4) <- evalRandTIO $ mutateAddNode i3 is3 genome3
+  (i5, is5, genome5) <- evalRandTIO $ mutateAddNode i4 is4 genome4
+  (i6, is6, genome6) <- evalRandTIO $ mutateAddNode i5 is5 genome5
+  putStrLn "====================="
+  putStrLn "addNode 1:"
+  showGenome genome4
+  putStrLn "addNode 2:"
+  showGenome genome5
+  putStrLn "addNode 3:"
+  showGenome genome6
+  genome7 <- evalRandTIO $ mutateWeights 0.2 genome6
+  genome8 <- evalRandTIO $ mutateWeights 0.2 genome7
+  putStrLn "====================="
+  putStrLn "mutate weights:"
+  showGenome genome7
+  putStrLn "mutate weights:"
+  showGenome genome8
+
+showGenome :: Genome -> IO ()
+showGenome genome = do
+  putStrLn $ "- inNodes = " ++ show (inNodes genome) ++ ", outNodes = " ++ show (outNodes genome)
+      ++ ", hiddenNodes = " ++ show (hiddenNodes genome)
+  forM_ (genes genome) $ \g -> putStrLn ("- " ++ show g)
--- library/Genome.hs
+++ library/Genome.hs
@@ -1,15 +1,15 @@
 module Genome (
-    Gene(..), Genome(..), Connection(..),
-    mutateAddConnection, mutateAddNode, closure, connectedIn
+    Gene(..), Genome(..), Connection(..), Innovations,
+    mutateAddConnection, mutateAddNode, mutateWeights,
+    closure, connectedIn
 ) where
 
 import Control.Monad.Random
 import Data.IntSet (IntSet, member, union, unions, singleton)
--- import qualified Data.IntSet as S
 
 {--
 The genotype is represented as:
-- the number of sensors (input), output and hidden nodes (there is always a bias "input", always 1)
+- the number of sensors (inputs), outputs and hidden nodes (there is always a bias "input", always 1)
 - a list of connecton genes, describing the connections of the network and its weights
 
 Connections go from one node to another with 2 restrictions:
@@ -28,34 +28,72 @@ data Genome = Genome {
                   genes :: [Gene]
             } deriving Show
 
+type Innovations = [((Int, Int), Int)]
+
+weightRange :: Float
+weightRange = 10  -- value range for new weights
+
+-- Reusing the innovation numbers facilitates the crossovers
+reuseInnovation :: Int -> Int -> (Int, Innovations) -> (Int, (Int, Innovations))
+reuseInnovation f t (inno, innos)
+    = case lookup (f, t) innos of
+          Nothing -> (inno, (inno + 1, ((f, t), inno) : innos)) -- consume
+          Just iv -> (iv,   (inno, innos))                      -- reuse
+
 -- We have to add connections in such a way that we do not create cycles
-mutateAddConnection :: (RandomGen g, Monad m) => Int -> Genome -> RandT g m (Maybe Genome)
-mutateAddConnection inno genome = do
+-- If we have chosen the wrong nodes, we may fail
+-- Same innovation in the same generation will get the same number (reuse)
+mutateAddConnection :: (RandomGen g, Monad m) => Int -> Innovations -> Genome
+                                              -> RandT g m (Maybe (Int, Innovations, Genome))
+mutateAddConnection inno innos genome = do
     let maf = inNodes genome + outNodes genome + hiddenNodes genome
         mit = inNodes genome + 1
         gs  = filter enabled (genes genome)
-    f <- getRandomR (0, maf)   -- every node can be source
+    f <- getRandomR (0,   maf) -- every node can be source
     t <- getRandomR (mit, maf) -- bias and inputs can't be destination
     if f == t || (f, t) `connectedIn` (genes genome) || f `member` (closure gs t)
        then return Nothing
        else do
-           w <- getRandomR (-1, 1)  -- which range should we take?
-           let genome2 = genome { genes = genes genome ++ [gene] }
-               gene = Gene { inNode = f, outNode = t, innov = inno, weight = w, enabled = True }
-           return (Just genome2)
+           w <- getRandomR (-weightRange, weightRange)
+           let (innog, (inno1, innos1)) = reuseInnovation f t (inno, innos)
+               gene = Gene { inNode = f, outNode = t, innov = innog, weight = w, enabled = True }
+               genome2 = genome { genes = gene : genes genome }
+           return $ Just (inno1, innos1, genome2)
 
-mutateAddNode :: (RandomGen g, Monad m) => Int -> Genome -> RandT g m Genome
-mutateAddNode inno genome = do
+-- Add node splits one connection in 2, with a new (hidden) node in between
+-- Only enabled connections can be chosen
+mutateAddNode :: (RandomGen g, Monad m) => Int -> Innovations -> Genome
+                                        -> RandT g m (Int, Innovations, Genome)
+mutateAddNode inno innos genome = do
     let cs = filter enabled (genes genome)
     co <- uniform cs
     let cd = co { enabled = False }
         (cs1, cs2) = span ((/= innov co) . innov) (genes genome)
         h = hiddenNodes genome + 1
         n = inNodes genome + outNodes genome + h
-        gene1 = co { outNode = n, innov = inno, weight = 1 }
-        gene2 = co { inNode = n, innov = inno + 1 }
+        (innog1, (inno1, innos1)) = reuseInnovation (inNode co) n  (inno,  innos)
+        (innog2, (inno2, innos2)) = reuseInnovation n (outNode co) (inno1, innos1)
+        gene1 = co { outNode = n, innov = innog1, weight = 1 }
+        gene2 = co { inNode = n,  innov = innog2 }
         genome2 = genome { hiddenNodes = h, genes = cs1 ++ [cd] ++ tail cs2 ++ [gene1, gene2] }
-    return genome2
+    return (inno2, innos2, genome2)
+
+-- Mutate weights: every weight gets a new value with some probability
+-- or just a perturbation of the current value (uniform from 0 to 200%)
+mutateWeights :: (RandomGen g, Monad m) => Float -> Genome -> RandT g m Genome
+mutateWeights newp genome = do
+    gs <- mapM (mutateOneWeight newp) (filter enabled $ genes genome)
+    return genome { genes = gs ++ filter (not . enabled) (genes genome) }
+
+mutateOneWeight :: (RandomGen g, Monad m) => Float -> Gene -> RandT g m Gene
+mutateOneWeight newp gene = do
+    new <- getRandomR (0, 1)
+    w <- if new >= newp
+            then getRandomR (-weightRange, weightRange) -- new value
+            else do
+                wp <- getRandomR (-1, 1)
+                return $! weight gene * (1 + wp) -- perturbation of old value
+    return gene { weight = w }
 
 -- The infrastructure to keep the graph acyclic
 class Connection a where
